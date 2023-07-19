7a7daadf3f143f0e01cb03e57204113fb1430e01
Brady Barker <brady.d.barker@gmail.com>
2023-07-18 20:20:32 -0400

    Added view to posts/new to create new post Added logic to posts_controller :new and :create

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,13 +1,28 @@
 class PostsController < ApplicationController
+    before_action :authenticate_user!, only: [:new, :create]
+
     def index 
 
     end
     
     def new
-
+        @post = Post.new
     end
 
     def create
+        @post = Post.new(post_params)
+        @post.user_id = current_user
 
+        if @post.save
+            redirect_to post_path
+        else
+            render :new
+        end
+    end
+
+    private
+
+    def post_params
+        params.require(:post).permit(:body)
     end
 end
