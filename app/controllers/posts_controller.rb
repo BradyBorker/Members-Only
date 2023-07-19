f02991d796f128add12ad01144c348ae8227b196
Brady Barker <brady.d.barker@gmail.com>
2023-07-18 21:20:22 -0400

    Project completed

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -23,6 +23,6 @@ class PostsController < ApplicationController
     private
 
     def post_params
-        params.require(:post).permit(:body)
+        params.require(:post).permit(:author, :body)
     end
 end
